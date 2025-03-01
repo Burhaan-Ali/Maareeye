88424e47abb25595fc0fff729ca800df26a50daf
burhaan <burhaancali784@gmail.com>
2025-03-01 20:41:43 +0300

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-let menu = document.querySelector("#menu")
-let menuIcon = document.querySelector("#menu-icon")
-menuIcon.addEventListener("click",()=>{
-    menu.classList.toggle("hidden")
-    menuIcon.children[0].classList.toggle("hidden")
-    menuIcon.children[1].classList.toggle("hidden")
+// navigation
+$("#menu-icon").on("click",()=>{
+    $("#menu").slideToggle("hidden")
+    $("#menu-bar").slideToggle("hidden")
+    $("#cancel-bar").slideToggle("hidden")
 })
 
 
@@ -36,7 +35,7 @@ const carCreator = (elementId,arr)=>{
 
 }
 
-
+// features
 const features = [
     {
         img:"img/feature1-01.png",
@@ -64,7 +63,7 @@ features.map((feature)=>{
     `
 })
 
-
+// testimonial
 const testmonials=[
     {
         style:"md:top-[20%] top-[10%] left-[63%]  hover:md:left-[63%]",
@@ -76,50 +75,73 @@ const testmonials=[
     {
         style:"top-[7%] left-[45%]  hover:md:left-[45%]",
         img:"img/3.jpg",
-        name:"Muse Ali",
+        name:"Liibaan Ciyaaye",
         type:"Customer",
         speech:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et iusto explicabo ducimus magnam fugit dolore eum, architecto hic nesciunt veniam incidunt, velit laboriosam. Minus, quod."
     },
     {
         style:"top-[25%] left-[25%]  hover:md:left-[25%]",
         img:"img/3.jpg",
-        name:"Muse Ali",
+        name:"Hamdi Muuse",
         type:"Customer",
         speech:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et iusto explicabo ducimus magnam fugit dolore eum, architecto hic nesciunt veniam incidunt, velit laboriosam. Minus, quod."
     },
     {
         style:"md:top-[25%] top-[50%] left-[55%]  hover:md:left-[55%]",
         img:"img/3.jpg",
-        name:"Muse Ali",
+        name:"Qaali Ali",
         type:"Customer",
         speech:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et iusto explicabo ducimus magnam fugit dolore eum, architecto hic nesciunt veniam incidunt, velit laboriosam. Minus, quod."
     },
     {
         style:"top-[60%] left-[20%]  hover:md:left-[20%]",
         img:"img/3.jpg",
-        name:"Muse Ali",
+        name:"abdulle mohamed",
         type:"Customer",
         speech:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et iusto explicabo ducimus magnam fugit dolore eum, architecto hic nesciunt veniam incidunt, velit laboriosam. Minus, quod."
     },
     {
         style:"top-[30%] left-[70%]  hover:md:left-[70%]",
         img:"img/3.jpg",
-        name:"Muse Ali",
+        name:"Fooleey nuurre",
         type:"Customer",
         speech:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et iusto explicabo ducimus magnam fugit dolore eum, architecto hic nesciunt veniam incidunt, velit laboriosam. Minus, quod."
     },
     {
-        style:"top-[20%] md:left-[10%]",
+        style:"top-[20%] md:left-[10%] hover:md:left-[10%] ",
         img:"img/3.jpg",
-        name:"Muse Ali",
+        name:"Dhaahir Bagaax",
         type:"Customer",
         speech:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et iusto explicabo ducimus magnam fugit dolore eum, architecto hic nesciunt veniam incidunt, velit laboriosam. Minus, quod."
     },
     {
         style:"md:top-[20%] top-[10%] left-[63%]  hover:md:left-[63%]",
         img:"img/3.jpg",
-        name:"Muse Ali",
+        name:"Ahmed Jamac",
         type:"Customer",
         speech:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et iusto explicabo ducimus magnam fugit dolore eum, architecto hic nesciunt veniam incidunt, velit laboriosam. Minus, quod."
     }
 ]
+testmonials.forEach((person)=>{
+    document.querySelector("#testmonials").innerHTML +=`
+                    <div  class="w-[70px] h-[70px] border p-2 hover:w-[400px] hover:h-[250px] hover:rounded-lg rounded-full overflow-hidden transition absolute ${person.style}  hover:left-[0%] bg-[#fff] hover:z-10">
+                <div class="flex">
+                    <img src="${person.img}" alt="" srcset="" class="w-[50px] h-[50px] rounded-full group">
+                    <div class="ms-4">
+                        <div class="font-bold">${person.name}</div>
+                        <div class="">${person.type}</div>
+                    </div>
+                </div>
+                <p class="text-gray-500 mt-7">
+                    ${person.speech}
+                </p>
+                <div class="grid grid-cols-5 gap-3 w-[100px] pt-3 text-yellow-500">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star-half"></i>
+                </div>
+            </div>
+    `
+})
